ecdc6dae8be285fe00ed24b9681a41c67809fdf8
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-08-16 00:20:04 +0200

    added /say

--- src/main/java/com/enderzombi102/minebot/command/SayCommand.java
+++ src/main/java/com/enderzombi102/minebot/command/SayCommand.java
@@ -2,11 +2,25 @@ package com.enderzombi102.minebot.command;
 
 import com.enderzombi102.minebot.api.command.CommandManager;
 import com.enderzombi102.minebot.api.command.CommandPreserverProvider;
+import com.enderzombi102.minebot.api.command.CommandTypes;
+import com.enderzombi102.minebot.api.exception.CommandAlreadyRegisteredException;
+import com.mojang.brigadier.arguments.StringArgumentType;
 
 public class SayCommand extends CommandPreserverProvider {
 
 	@Override
-	protected void register(CommandManager manager) {
-		
+	protected void register(CommandManager manager) throws CommandAlreadyRegisteredException {
+		manager.RegisterCommand(
+				CommandTypes.literal("say").then(
+						CommandTypes.argument("string", StringArgumentType.string() ).executes(
+								ctx -> {
+									ctx.getSource().getChannel().sendMessage(
+											ctx.getArgument("string", String.class)
+									).queue();
+									return 0;
+								}
+						)
+				)
+		);
 	}
 }
