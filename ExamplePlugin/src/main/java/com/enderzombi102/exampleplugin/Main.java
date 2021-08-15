9dcb2064e50a483477923ac65dce66edab38b765
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2021-08-16 00:22:16 +0200

    updated example code

--- ExamplePlugin/src/main/java/com/enderzombi102/exampleplugin/Main.java
+++ ExamplePlugin/src/main/java/com/enderzombi102/exampleplugin/Main.java
@@ -1,6 +1,7 @@
 package com.enderzombi102.exampleplugin;
 
 import com.enderzombi102.minebot.api.command.CommandManager;
+import com.enderzombi102.minebot.api.command.CommandProvider;
 import com.enderzombi102.minebot.api.command.CommandTypes;
 import com.enderzombi102.minebot.api.MineBot;
 import com.enderzombi102.minebot.api.exception.CommandAlreadyRegisteredException;
@@ -8,32 +9,38 @@ import com.enderzombi102.minebot.api.plugsys.Plugin;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main implements Plugin {
+
+	private final ArrayList<CommandProvider> providers = new ArrayList<>();
+
 	@Override
 	public void onLoad() {
 		Logger logger = getLogger();
-		CommandManager comManager = MineBot.getInstance().getManager("command");
-		assert comManager != null;
-		try {
-			comManager.RegisterCommand(
-					CommandTypes.literal("say").then(
-							CommandTypes.argument("string", StringArgumentType.string()).executes(
-									ctx -> {
-										ctx.getSource().getChannel().sendMessage(
-												ctx.getArgument("string", String.class)
-										).queue();
-										return 0;
-									}
-							)
-					)
-			);
-		} catch (CommandAlreadyRegisteredException e) {
-			logger.error("Failed to register commands!", e);
-		}
+		logger.info("ExamplePlugin loaded!");
+		providers.add( manager -> manager.TryRegisterCommand(
+				CommandTypes.literal("say").then(
+						CommandTypes.argument("string", StringArgumentType.string()).executes(
+								ctx -> {
+									ctx.getSource().getChannel().sendMessage(
+											ctx.getArgument("string", String.class)
+									).queue();
+									return 0;
+								}
+						)
+				)
+		));
 	}
 
 	@Override
 	public void onUnload() {
 
+	}
+
+	@Override
+	public List<CommandProvider> getProviders() {
+		return providers;
 	}
 }
